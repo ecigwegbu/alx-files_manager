17f30c184f00a7f84c588b87df125c9fc9cb0dcf
ecigwegbu <igwegbu@gmail.com>
2023-09-03 21:15:35 +0000

    2. First API

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ import routes from './routes';
 const app = express();
 app.use(express.json());
 app.use('/', routes);
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 5000;
 
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
