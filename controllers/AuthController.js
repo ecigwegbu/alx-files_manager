b1cfe071338330cbc66e01535effb458f6c90593
ecigwegbu <igwegbu@gmail.com>
2023-09-04 15:58:38 +0000

    4. Authenticate a user

--- controllers/AuthController.js
+++ controllers/AuthController.js
@@ -1,5 +1,6 @@
 // contains the definitions of /users endpoint
 import { v4 as uuidv4 } from 'uuid';
+import { ObjectId } from 'mongodb';
 import sha1 from 'sha1';
 import redisClient from '../utils/redis';
 import dbClient from '../utils/db';
@@ -27,7 +28,6 @@ const getConnect = async (req, res) => {
     let user;
     try {
       user = await dbClient.db.collection('users').findOne({ email });
-      console.log('-->-->', user);
       if (!user) {
         res.status(401).send({ error: 'Unauthorized' });
         return;
@@ -38,7 +38,6 @@ const getConnect = async (req, res) => {
     }
 
     // check password
-    console.log('stored:', user.password);
     const storedHashedPassword = user.password;
     const testHashedPassword = sha1(`${password}`);
     if (storedHashedPassword !== testHashedPassword) {
@@ -63,9 +62,43 @@ const getConnect = async (req, res) => {
 
 const getDisconnect = async (req, res) => {
   if (dbsAlive()) {
-    const { email, password } = req.body;
-    // respond to client
-    // res.status(201).send({ id: `${result.insertedId}`, email });
+    // get authorisation header
+    const sessionHeader = req.headers['x-token'];
+    // console.log('req Header:', req.headers['X-Token']);
+
+    // Decode and parse the Authorization header
+    if (!sessionHeader) {
+      res.status(401).send({ error: 'Unauthorized' });
+      return;
+    }
+    let userId;
+    try {
+      const userId = await redisClient.get(`auth_${sessionHeader.token}`);
+      if (!userId) {
+        res.status(401).send({ error: 'Unauthorized' });
+        return;
+      }
+    } catch (err) {
+      res.status(500).send({ error: 'Redis Get Error' });
+      return;
+    }
+    // now you have a valid userId and token!
+    // delete key from Redis:
+
+    try {
+      await redisClient.del(`auth_${sessionHeader.token}`);
+    } catch (err) {
+      res.status(500).send({ error: 'Redis Del Error' });
+      return;
+    }
+    // delete user from MongoDB
+    try {
+      await dbClient.db.collection('users').deleteOne({ _id: new ObjectId(userId) });
+    } catch (err) {
+      res.status(500).send({ error: 'MongoDB Del Error' });
+      return;
+    }
+    res.status(204).end();
   } else {
     res.status(500).send({ error: 'Database not alive' });
   }
@@ -73,43 +106,45 @@ const getDisconnect = async (req, res) => {
 
 const getMe = async (req, res) => {
   if (dbsAlive()) {
-    const { email, password } = req.body;
-    // respond to client
-    // res.status(201).send({ id: `${result.insertedId}`, email });
+    // get authorisation header
+    const sessionHeader = req.headers['x-token'];
+    // console.log('req Header:', req.headers['X-Token']);
+
+    // Check if header present
+    if (!sessionHeader) {
+      res.status(401).send({ error: 'Unauthorized' });
+      return;
+    }
+    // if token matches retrieve userId
+    let userId;
+    // console.log('-->--> sessionHeader', sessionHeader);
+    // console.log('-->--> sessionHeader.token', sessionHeader.token);
+    try {
+      userId = await redisClient.get(`auth_${sessionHeader}`);
+      // console.log('-->type of userid:', typeof userId);
+      if (!userId) {
+        res.status(401).send({ error: 'Unauthorized' });
+        return;
+      }
+    } catch (err) {
+      res.status(500).send({ error: 'Redis Get Error' });
+      return;
+    }
+    // now you have a valid userId and token!
+    // retrieve user from MongoDB
+    // console.log('-->userId:', userId);
+    // console.log(',,,,,,,Object id:',  new ObjectId(userId));
+    let user;
+    try {
+      user = await dbClient.db.collection('users').findOne({ _id: new ObjectId(userId) });
+    } catch (err) {
+      res.status(500).send({ error: 'MongoDB Search Error' });
+    }
+    // console.log('-->user:', user);
+    res.status(200).send({ id: userId, email: user.email });
   } else {
     res.status(500).send({ error: 'Database not alive' });
   }
 };
 
 export { getConnect, getDisconnect, getMe };
-/*
-    // check if email or password missing
-    if (!email) {
-      res.status(400).send({ error: 'Missing email' });
-      res.end();
-      return;
-    }
-    if (!password) {
-      res.status(400).send({ error: 'Missing password' });
-      return;
-    }
-    // check for existing user
-    try {
-      const user = await dbClient.db.collection('users').findOne({ email });
-      if (user) {
-        res.status(400).send({ error: 'Already exist' });
-        return;
-      }
-    } catch (err) {
-      res.status(500).send({ error: 'User Collection error' });
-      return;
-    }
-    // hash password
-    // const salt = 'GYt59Hvfgwq+tPut6-iuytrdseboy';
-    // const hashedPassword = sha1(`${password}${salt}`);
-    const hashedPassword = sha1(`${password}`);
-    // add user to db
-    const result = await dbClient.db.collection('users').insertOne(
-      { email, password: hashedPassword },
-    );
-*/
