4ad29190f1144ffd1f69f79ffc50a34d7c6c8ade
ecigwegbu <igwegbu@gmail.com>
2023-09-04 09:41:11 +0000

    4. Authenticate a user

---
+++ controllers/AuthController.js
@@ -0,0 +1,73 @@
+// contains the definitions of /users endpoint
+import sha1 from 'sha1';
+import redisClient from '../utils/redis';
+import dbClient from '../utils/db';
+
+const dbsAlive = () => (redisClient.isAlive() && dbClient.isAlive());
+
+const getConnect = async (req, res) => {
+  if (dbsAlive()) {
+    const { email, password } = req.body;
+    // respond to client
+    // res.status(201).send({ id: `${result.insertedId}`, email });
+  } else {
+    res.status(500).send({ error: 'Database not alive' });
+  }
+};
+
+
+const getDisconnect = async (req, res) => {
+  if (dbsAlive()) {
+    const { email, password } = req.body;
+    // respond to client
+    // res.status(201).send({ id: `${result.insertedId}`, email });
+  } else {
+    res.status(500).send({ error: 'Database not alive' });
+  }
+};
+
+const getMe = async (req, res) => {
+  if (dbsAlive()) {
+    const { email, password } = req.body;
+    // respond to client
+    // res.status(201).send({ id: `${result.insertedId}`, email });
+  } else {
+    res.status(500).send({ error: 'Database not alive' });
+  }
+};
+
+export { getConnect, getDisconnect, getMe };
+/*
+
+
+
+    // check if email or password missing
+    if (!email) {
+      res.status(400).send({ error: 'Missing email' });
+      res.end();
+      return;
+    }
+    if (!password) {
+      res.status(400).send({ error: 'Missing password' });
+      return;
+    }
+    // check for existing user
+    try {
+      const user = await dbClient.db.collection('users').findOne({ email });
+      if (user) {
+        res.status(400).send({ error: 'Already exist' });
+        return;
+      }
+    } catch (err) {
+      res.status(500).send({ error: 'User Collection error' });
+      return;
+    }
+    // hash password
+    // const salt = 'GYt59Hvfgwq+tPut6-iuytrdseboy';
+    // const hashedPassword = sha1(`${password}${salt}`);
+    const hashedPassword = sha1(`${password}`);
+    // add user to db
+    const result = await dbClient.db.collection('users').insertOne(
+      { email, password: hashedPassword },
+    );
+*/
--- routes/index.js
+++ routes/index.js
@@ -2,11 +2,15 @@
 import express from 'express';
 import { getStatus, getStats } from '../controllers/AppController';
 import postNew from '../controllers/UsersController';
+import { getConnect, getDisconnect, getMe } from '../controllers/AuthController';
 
 const routes = express.Router();
 
 routes.get('/status', getStatus);
 routes.get('/stats', getStats);
 routes.post('/users', postNew);
+routes.get('/connect', getConnect);
+routes.get('/disconnect', getDisconnect);
+routes.get('/users/me', getMe);
 
 module.exports = routes;
