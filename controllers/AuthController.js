72059132e6d412dc5de5e2e4180def206f24fafb
ecigwegbu <igwegbu@gmail.com>
2023-09-04 12:46:12 +0000

    4. Authenticate a user

--- controllers/AuthController.js
+++ controllers/AuthController.js
@@ -1,4 +1,5 @@
 // contains the definitions of /users endpoint
+import { v4 as uuidv4 } from 'uuid';
 import sha1 from 'sha1';
 import redisClient from '../utils/redis';
 import dbClient from '../utils/db';
@@ -7,14 +8,58 @@ const dbsAlive = () => (redisClient.isAlive() && dbClient.isAlive());
 
 const getConnect = async (req, res) => {
   if (dbsAlive()) {
-    const { email, password } = req.body;
-    // respond to client
-    // res.status(201).send({ id: `${result.insertedId}`, email });
+    // get authorisation header
+    const authHeader = req.headers.authorization;
+
+    // Decode and parse the Authorization header
+    if (!authHeader) {
+      res.status(401).send({ error: 'Unauthorized' });
+      return;
+    }
+    const [email, password] = Buffer.from(authHeader.split(' ')[1], 'base64')
+      .toString()
+      .split(':');
+    if (!email || !password) {
+      res.status(401).send({ error: 'Unauthorized' });
+      return;
+    }
+    // check for existing user
+    let user;
+    try {
+      user = await dbClient.db.collection('users').findOne({ email });
+      console.log('-->-->', user);
+      if (!user) {
+        res.status(401).send({ error: 'Unauthorized' });
+        return;
+      }
+    } catch (err) {
+      res.status(500).send({ error: 'User Collection error' });
+      return;
+    }
+
+    // check password
+    console.log('stored:', user.password);
+    const storedHashedPassword = user.password;
+    const testHashedPassword = sha1(`${password}`);
+    if (storedHashedPassword !== testHashedPassword) {
+      res.status(401).send({ error: 'Unauthorized' });
+      return;
+    }
+    // user logged in as user with email and password
+    const token = uuidv4();
+    const key = `auth_${token}`;
+    try {
+      await redisClient.set(key, user._id.toString(), 24 * 60 * 60);
+    } catch (err) {
+      res.status(500).send({ error: 'Redis Set Error' });
+      return;
+    }
+    // res.status(200).send({ token: `${token}` });
+    res.status(200).send({ token });
   } else {
     res.status(500).send({ error: 'Database not alive' });
   }
 };
-
 
 const getDisconnect = async (req, res) => {
   if (dbsAlive()) {
@@ -38,9 +83,6 @@ const getMe = async (req, res) => {
 
 export { getConnect, getDisconnect, getMe };
 /*
-
-
-
     // check if email or password missing
     if (!email) {
       res.status(400).send({ error: 'Missing email' });
