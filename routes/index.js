797d28967b043030e69cc89f16b38801a5ce2c28
ecigwegbu <igwegbu@gmail.com>
2023-09-06 08:10:24 +0000

    8. File data

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,7 @@ import { getStatus, getStats } from '../controllers/AppController';
 import postNew from '../controllers/UsersController';
 import { getConnect, getDisconnect, getMe } from '../controllers/AuthController';
 import {
-  postUpload, getShow, getIndex, putPublish, putUnpublish,
+  postUpload, getShow, getIndex, putPublish, putUnpublish, getFile,
 } from '../controllers/FilesController';
 
 const routes = express.Router();
@@ -20,5 +20,6 @@ routes.get('/files/:id', getShow);
 routes.get('/files', getIndex);
 routes.put('/files/:id/publish', putPublish);
 routes.put('/files/:id/unpublish', putUnpublish);
+routes.get('/files/:id/data', getFile);
 
 module.exports = routes;
